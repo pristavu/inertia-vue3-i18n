e08aed915833bbeee45d6a25509a1a2361934bfc
Andrei Pristavu <andrew@andrew.ro>
2022-11-09 10:04:36 +0200

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import { trans } from 'laravel-vue-i18n';
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+})
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</Link>
+
+            <template v-else>
+                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
+
+                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
+            </template>
+        </div>
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2 p-12">
+                    <h1>{{ $t('Welcome, :name!', { name: 'Francisco' }) }}. </h1>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</template>
---
+++ resources/js/ssr.js
@@ -0,0 +1,34 @@
+import {createSSRApp, h} from 'vue';
+import {renderToString} from '@vue/server-renderer';
+import {createInertiaApp} from '@inertiajs/inertia-vue3';
+import createServer from '@inertiajs/server';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {i18nVue} from 'laravel-vue-i18n'
+
+const appName = 'Laravel';
+
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+        setup({app, props, plugin}) {
+            return createSSRApp({render: () => h(app, props)})
+
+                .use(plugin)
+                .use(ZiggyVue, {
+                    ...page.props.ziggy,
+                    location: new URL(page.props.ziggy.location),
+                })
+                .use(i18nVue, {
+                    lang: 'pt',
+                    resolve: lang => {
+                        const langs = import.meta.globEager('../../lang/*.json');
+                        return langs[`../../lang/${lang}.json`].default;
+                    },
+                });
+        },
+    })
+);
